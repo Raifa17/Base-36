7b3026ce1eac0146e88430f96b395e067adbd69f
Alex Vidal <alejandrovid17@hotmail.com>
2018-03-04 21:18:00 +0100

    Cambios

---
+++ Conversor/Conversor/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conversor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(args[0]);
+            string caracter = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+            if (num < 0)
+            {
+                Console.WriteLine("Introduce un numero mayor a 0");
+            }
+            else
+            {
+                int residuo = num % 36;
+                string resultado2 = "";
+                resultado2 = caracter[residuo] + resultado2;
+                while (num > 36)
+                {
+                    residuo = num % 36;
+                    num = residuo;
+                    resultado2 = caracter[residuo] + resultado2="";
+                }
+                Console.WriteLine("Tu alfanumerico es " + resultado2);
+            }
+        }
+            
+    }
+}
